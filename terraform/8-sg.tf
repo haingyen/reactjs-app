d621f4f8083f133fbfb8d3efc4bdc6f3f7c6a38e
26nch <nchb1807838@gmail.com>
2025-06-12 22:10:34 +0700

    update security group

--- terraform/8-sg.tf
+++ terraform/8-sg.tf
@@ -19,6 +19,20 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTPS from Internet"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
@@ -44,7 +58,8 @@ resource "aws_security_group" "sg_for_ec2_instance" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    # cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb_sg.id]  # Chỉ cho phép traffic từ ALB
   }
   ingress {
     from_port = 8080
